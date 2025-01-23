d62c47b05b4d6b57da6b57af7968c59606a4deb3
Gina Yuan <gina.yuan@gmail.com>
2025-01-23 13:17:14 -0500

    Test static methods of EmulatedNetwork

--- emulation/tests/test_network.py
+++ emulation/tests/test_network.py
@@ -10,50 +10,34 @@ class TestNetStatistics(unittest.TestCase):
         pass
 
 
-class TestOneHopNetwork(unittest.TestCase):
-    def setUp(self):
-        pass
-
+class TestEmulatedNetwork(unittest.TestCase):
     def test_ip(self):
-        self.assertEqual(OneHopNetwork._ip(1), '10.0.1.10/24')
-        self.assertEqual(OneHopNetwork._ip(2), '10.0.2.10/24')
-        self.assertEqual(OneHopNetwork._ip(0), '10.0.0.10/24')
-        self.assertEqual(OneHopNetwork._ip(9), '10.0.9.10/24')
+        self.assertEqual(EmulatedNetwork._ip(1), '172.16.1.10/24')
+        self.assertEqual(EmulatedNetwork._ip(2), '172.16.2.10/24')
+        self.assertEqual(EmulatedNetwork._ip(0), '172.16.0.10/24')
+        self.assertEqual(EmulatedNetwork._ip(9), '172.16.9.10/24')
         with self.assertRaises(AssertionError):
-            OneHopNetwork._ip(10)
+            EmulatedNetwork._ip(10)
         with self.assertRaises(AssertionError):
-            OneHopNetwork._ip(-1)
+            EmulatedNetwork._ip(-1)
 
     def test_mac(self):
-        self.assertEqual(OneHopNetwork._mac(1), '00:00:00:00:00:01')
-        self.assertEqual(OneHopNetwork._mac(2), '00:00:00:00:00:02')
-        self.assertEqual(OneHopNetwork._mac(0), '00:00:00:00:00:00')
-        self.assertEqual(OneHopNetwork._mac(9), '00:00:00:00:00:09')
+        self.assertEqual(EmulatedNetwork._mac(1), '00:00:00:00:00:01')
+        self.assertEqual(EmulatedNetwork._mac(2), '00:00:00:00:00:02')
+        self.assertEqual(EmulatedNetwork._mac(0), '00:00:00:00:00:00')
+        self.assertEqual(EmulatedNetwork._mac(9), '00:00:00:00:00:09')
         with self.assertRaises(AssertionError):
-            OneHopNetwork._mac(10)
+            EmulatedNetwork._mac(10)
         with self.assertRaises(AssertionError):
-            OneHopNetwork._mac(-1)
+            EmulatedNetwork._mac(-1)
 
     def test_calculate_bdp(self):
-        pass
-
-    def test_hosts_exist_and_are_reachable(self):
-        pass
-
-    def test_delay_config(self):
-        pass
-
-    def test_bandwidth_config(self):
-        pass
-
-    def test_loss_config(self):
-        pass
-
-    def test_queue_size_config(self):
-        pass
-
-    def test_popen(self):
-        pass
-
-    def test_stop(self):
-        pass
+        delay1 = 20 # ms
+        delay2 = 10
+        bw1 = 10 # Mbit/s
+        bw2 = 1000
+        expected_mbits = 0.6 # 60ms * 10Mbit/s
+        expected_bytes = expected_mbits * 1000000 / 8
+        actual_bytes = EmulatedNetwork._calculate_bdp(delay1, delay2, bw1, bw2)
+        self.assertEqual(actual_bytes, expected_bytes,
+                         'calculated bdp in bytes')
