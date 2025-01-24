StanfordSNR/sidekick-downloads

commit 5ddcac2ec2b40f0b5bb641b7fbda1b9c1aca2e3f

Fix bug in read_subprocess_pipe by reading all available lines in a ready stream
This fixes synchrononus tests that execute a callback on subprocess
output

emulation/common.py
@@ -49,10 +49,8 @@ def read_subprocess_pipe(p):
     while p.poll() is None:
         ready, _, _ = select.select([p.stdout, p.stderr], [], [])
         for stream in ready:
-            line = stream.readline()
-            if not line:
-                continue
-            yield (line, stream)
+            for line in stream.readlines():
+                yield (line, stream)
     stdout, stderr = p.communicate()
     for line in stdout.splitlines(keepends=True):
         yield (line, p.stdout)

emulation/tests/test_common.py
@@ -1,0 +1,16 @@
+import unittest
+import subprocess
+from common import *
+
+
+class TestSubprocessHandling(unittest.TestCase):
+	def test_reads_all_subprocess_output(self):
+		n = 10
+		cmd = ['seq', str(n)]
+		p = subprocess.Popen(cmd, text=True, stdout=subprocess.PIPE,
+			stderr=subprocess.PIPE)
+		output = []
+		for line, _ in read_subprocess_pipe(p):
+			output.append(line.strip())
+		self.assertEqual(len(output), n, output)
+		self.assertEqual(p.wait(), 0)

commit b4e51fc9dcf149911b746a7cb0f36a76e474bff6

Fix read_subprocess_pipe bug introduced in 5ddcac2
Calling readlines() on the stream ensures all output is processed,
but it's a blocking op. As a result, we couldn't receive the
notification about server readiness when running the TCP benchmark
from the CLI. This fix continues to read only single lines when
select indicates a stream is ready, but still calls readlines()
when the process is finished.

emulation/common.py
@@ -46,11 +46,17 @@ def init_logdir(path):
             pass
 
 def read_subprocess_pipe(p):
+    streams = [p.stdout, p.stderr]
     while p.poll() is None:
-        ready, _, _ = select.select([p.stdout, p.stderr], [], [])
+        ready, _, _ = select.select(streams, [], [])
         for stream in ready:
-            for line in stream.readlines():
-                yield (line, stream)
+            line = stream.readline()
+            if not line:
+                continue
+            yield (line, stream)
+    for stream in streams:
+        for line in stream.readlines():
+            yield (line, stream)
     stdout, stderr = p.communicate()
     for line in stdout.splitlines(keepends=True):
         yield (line, p.stdout)