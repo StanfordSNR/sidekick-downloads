cdb15515069dcfa889574fef92cea15bc2f318af
Thea Rossman <thearossman@gmail.com>
2025-01-24 19:02:07 +0000

    improve proxy_tester script for debugging

--- emulation/tests/proxy_tester.py
+++ emulation/tests/proxy_tester.py
@@ -8,6 +8,8 @@ from mininet.link import TCLink
 from mininet.cli import CLI
 
 import argparse
+import subprocess
+import os
 
 # Configure bridging on r2
 def start_bridge(p1):
@@ -15,20 +17,22 @@ def start_bridge(p1):
     p1.cmd('sudo ip link set p1-eth1 promisc on')
     p1.cmd('sudo ip link add name br0 type bridge')
     p1.cmd('sudo ip link set p1-eth0 master br0 && sudo ip link set p1-eth1 master br0')
-    p1.cmd('sudo ip link set dev br0 learning off') # not needed?
     p1.cmd('sudo ip link set br0 up')
 
-def start_pcap(hosts, p1):
-    outdir = '/tmp/proxy_pcaps'
-    os.mkdir(outdir)
+outdir = '/tmp/proxy_pcaps'
+def start_pcap(hosts, p1, bridge):
+    suffix = 'bridge' if bridge else 'proxy'
+    print(f'Saving pcaps ({suffix}) to {outdir}')
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
     for host in hosts:
-        hosts[host].cmd(f'tcpdump -i {host}-eth0 inbound -w {outdir}/{host}-eth0-in.pcap -v &')
-        hosts[host].cmd(f'tcpdump -i {host}-eth0 outbound -w {outdir}/{host}-eth0-out.pcap -v &')
+        hosts[host].cmd(f'tcpdump -i {host}-eth0 inbound -w {outdir}/{host}-eth0-in-{suffix}.pcap -v &')
+        hosts[host].cmd(f'tcpdump -i {host}-eth0 outbound -w {outdir}/{host}-eth0-out-{suffix}.pcap -v &')
 
-    p1.cmd(f'tcpdump -i p1-eth0 inbound -w {outdir}/p1-eth0-in.pcap -v &')
-    p1.cmd(f'tcpdump -i p1-eth1 inbound -w {outdir}/p1-eth1-in.pcap -v &')
-    p1.cmd(f'tcpdump -i p1-eth0 outbound -w {outdir}/p1-eth0-out.pcap -v &')
-    p1.cmd(f'tcpdump -i p1-eth1 outbound -w {outdir}/p1-eth1-out.pcap -v &')
+    p1.cmd(f'tcpdump -i p1-eth0 inbound -w {outdir}/p1-eth0-in-{suffix}.pcap -v &')
+    p1.cmd(f'tcpdump -i p1-eth1 inbound -w {outdir}/p1-eth1-in-{suffix}.pcap -v &')
+    p1.cmd(f'tcpdump -i p1-eth0 outbound -w {outdir}/p1-eth0-out-{suffix}.pcap -v &')
+    p1.cmd(f'tcpdump -i p1-eth1 outbound -w {outdir}/p1-eth1-out-{suffix}.pcap -v &')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -39,6 +43,10 @@ if __name__ == '__main__':
                         help='Run bridging baseline (vs. proxy)')
     parser.add_argument('--pcap', action='store_true',
                         help='Take pcaps on all interfaces')
+    parser.add_argument('--cli', action='store_true',
+                        help='Open CLI (rather than iperf)')
+    parser.add_argument('--ping', action='store_true',
+                        help='Run ping (rather than iperf)')
     args = parser.parse_args()
 
     # Create network: h1 -> p1 -> h2
@@ -63,9 +71,34 @@ if __name__ == '__main__':
 
     # Start pcaps
     if args.pcap:
-        start_pcap({'h1': h1, 'h2': h2}, p1)
+        start_pcap({'h1': h1, 'h2': h2}, p1, args.bridge)
 
-    # Run CLI for testing
-    CLI(net)
+    if args.ping:
+        print("Running ping")
+        outp = h1.cmd(f'ping {h2.IP()} -c 10 -i 0.5')
+        print(outp)
+
+    if args.cli:
+        CLI(net)
+    elif not args.ping:
+        print('h2: iperf3 -s &')
+        output = h2.cmd('iperf3 -s -p 5201 &')
+        h1_cmd = f'timeout 5 iperf3 -c {h2.IP()} -p 5201'
+        print(h1_cmd)
+        output = h1.cmd(h1_cmd)
+        print('iperf3 output: ' + output)
+
+    h2.cmd('killall iperf3')
     net.stop()
+
+    if args.pcap:
+        suffix = 'bridge' if args.bridge else 'proxy'
+        print('fist 10 packets captured on h2-inbound:')
+        res = subprocess.run(['tcpdump', '-r', f'{outdir}/h2-eth0-in-{suffix}.pcap', '-c', '10'],
+                             capture_output=True, text=True)
+        print(res.stdout)
+        print('first 10 packets captured on h2-outbound:')
+        res = subprocess.run(['tcpdump', '-r', f'{outdir}/h2-eth0-out-{suffix}.pcap', '-c', '10'],
+                             capture_output=True, text=True)
+        print(res.stdout)
 
