2292fae5cf3f4aaf215659a60a1d435b340dcfcd
Thea Rossman <thearossman@gmail.com>
2025-02-13 22:29:53 +0000

    add discovery op type to support ACKs

--- sidekick_utils/src/discovery.rs
+++ sidekick_utils/src/discovery.rs
@@ -4,12 +4,22 @@ use log::trace;
 
 const MAGIC: [u8; 6] = *b"SKDISC";
 
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Clone)]
+pub enum DiscoveryOp {
+    Discover = 0,
+    DiscoverAck = 1,
+    Teardown = 2,
+    TeardownAck = 3,
+}
+
 /// The first packet on a sidekick connection.
 /// Should identify the corresponding base connection.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DiscoveryPayload {
     /// Identifies the packet as Sidekick Discovery.
     pub magic: [u8; 6],
+    /// Identifies the discovery packet type.
+    pub op: DiscoveryOp,
     /// Four-tuple (src_ip, src_port, dst_ip, dst_port) of the
     /// base connection from the perspective of the server (sender).
     /// Fields should be in NBO.
@@ -19,13 +29,15 @@ pub struct DiscoveryPayload {
 impl DiscoveryPayload {
     pub fn new(
         base_connection_stoc: AddrKey,
+        op: DiscoveryOp
     ) -> Self {
-        trace!("Creating new DiscoveryPayload for base connection {}",
+        trace!("Creating new DiscoveryPayload for base connection {}, {:?}",
                base_connection_stoc.iter()
                                    .map(|b| format!("{:02x}", b))
-                                   .collect::<String>());
+                                   .collect::<String>(), op);
         Self {
             magic: MAGIC,
+            op,
             base_connection_stoc,
         }
     }
