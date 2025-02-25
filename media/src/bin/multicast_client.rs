a5d34647f352ea6ed0f2fd470a8e6d0e3ea2fd32
Gina Yuan <gina.yuan@gmail.com>
2025-02-25 17:16:28 -0500

    Unicast implementation of multiple client single server scenario

---
+++ media/src/bin/multicast_client.rs
@@ -0,0 +1,166 @@
+use std::io;
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+use clap::Parser;
+use log::{debug, info};
+use tokio::task;
+use tokio::sync::Mutex;
+use tokio::net::UdpSocket;
+use tokio::time::{Instant, Duration};
+
+use media::{Packet, BufferedPackets, Statistics};
+use media::{PAYLOAD_SIZE, NACK_PAYLOAD_SIZE, TIMEOUT_SEQNO};
+
+
+#[derive(Debug, Parser)]
+struct Cli {
+    /// The unique client ID.
+    #[arg(long)]
+    client_id: String,
+    /// The NACK frequency, in ms. Typically the end-to-end RTT.
+    #[arg(long)]
+    nack_frequency: u64,
+    /// The delay to wait after detecting loss before sending a NACK, in ms.
+    #[arg(long, default_value_t = 0)]
+    nack_delay: u64,
+    /// Number of seconds to stream data before sending a timeout message.
+    #[arg(long, default_value_t = 60)]
+    timeout: u64,
+    /// Address of the other endpoint to send data to.
+    #[arg(long, default_value = "172.16.2.10:5201")]
+    addr: SocketAddr,
+}
+
+/// Listen for incoming packets on the UDP socket and handle.
+async fn listen_incoming(
+    sock: Arc<UdpSocket>, nack_frequency: Duration, nack_delay: Option<Duration>,
+    timeout: Duration, client_id: String,
+) -> io::Result<()> {
+    let mut buf = [0u8; PAYLOAD_SIZE];
+    let mut connection = None;
+    let start = Instant::now();
+    loop {
+        let (len, addr) = sock.recv_from(&mut buf).await?;
+        let data = Packet::from_payload(&buf);
+
+        // Handle non-data packets.
+        assert!(len == PAYLOAD_SIZE || len == NACK_PAYLOAD_SIZE);
+        if len == NACK_PAYLOAD_SIZE {
+            assert!(!data.is_nack);
+            assert!(!data.is_init);
+            if data.is_init_ack {
+                continue;
+            }
+        }
+
+        // Initialize the connection with the first data packet.
+        if connection.is_none() {
+            let stats = Statistics::new();
+            let buffer = BufferedPackets::new(data.seqno);
+            connection = Some((addr, stats, buffer));
+        }
+        let (from_addr, ref mut stats, ref mut buffer) = connection.as_mut().unwrap();
+        assert_eq!(*from_addr, addr);
+
+        // Add the data packet to the dejitter buffer and try to play data.
+        assert_ne!(data.seqno, TIMEOUT_SEQNO);
+        let now = Instant::now();
+        if buffer.recv_seqno(data.seqno, now) {
+            stats.add_spurious();
+        }
+        debug!("receive data {}", data.seqno);
+        while let Some(time_recv) = buffer.pop_seqno() {
+            stats.add_value(now - time_recv);
+        }
+
+        // Send NACKs for missing data.
+        for seqno in buffer.nacks_to_send(now, nack_frequency, nack_delay) {
+            debug!("nack {}", seqno);
+            let nack = Packet::new_nack(seqno);
+            let len = nack.fill_payload(&mut buf);
+            sock.send(&buf[..len]).await.unwrap();
+        }
+
+        // Check for timeout.
+        if now >= start + timeout {
+            let prefix = format!("[ID{}] ", client_id);
+            stats.print_statistics(Some(prefix));
+            break;
+        }
+    }
+    Ok(())
+}
+
+/// Send the initial message. Do it a bunch and hope one makes it through.
+async fn init_connection(
+    sock: Arc<UdpSocket>, init_frequency: Duration,
+) -> io::Result<()> {
+    let discovered = Arc::new(Mutex::new(false));
+    let mut payload = [0xFF; PAYLOAD_SIZE];
+    let mut interval = tokio::time::interval(init_frequency);
+
+    {
+        let sock = sock.clone();
+        let discovered = discovered.clone();
+        task::spawn(async move {
+            let mut payload = [0xFF; PAYLOAD_SIZE];
+            loop {
+                let len = sock.recv(&mut payload).await.unwrap();
+                if len != NACK_PAYLOAD_SIZE {
+                    continue;
+                }
+                let packet = Packet::from_payload(&payload);
+                if packet.is_init_ack {
+                    *discovered.lock().await = true;
+                    break;
+                }
+            }
+        });
+    }
+
+    loop {
+        interval.tick().await;
+        if *discovered.lock().await {
+            break;
+        }
+        let init = Packet::new_init();
+        let len = init.fill_payload(&mut payload);
+        sock.send(&payload[..len]).await.unwrap();
+    }
+    Ok(())
+}
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> io::Result<()> {
+    env_logger::init();
+    let args = Cli::parse();
+    let nack_frequency = Duration::from_millis(args.nack_frequency);
+
+    // Bind to the local socket to listen to and send packets from.
+    let sock = Arc::new(UdpSocket::bind("0.0.0.0:0").await?);
+    sock.connect(args.addr).await?;
+    info!("Ready to accept incoming packets {:?}", sock.local_addr());
+
+    // Initialize the connection.
+    {
+        let sock = sock.clone();
+        init_connection(sock, nack_frequency).await?;
+        info!("Connected to the server");
+    }
+
+    // Start the client.
+    {
+        let sock = sock.clone();
+        let timeout = Duration::from_secs(args.timeout);
+        let nack_delay = if args.nack_delay > 0 {
+            Some(Duration::from_millis(args.nack_delay))
+        } else {
+            None
+        };
+        listen_incoming(
+            sock, nack_frequency, nack_delay, timeout, args.client_id,
+        ).await?;
+    }
+    Ok(())
+}
---
+++ media/src/bin/multicast_server.rs
@@ -0,0 +1,131 @@
+use std::io;
+use std::net::SocketAddr;
+use std::collections::HashSet;
+use std::sync::Arc;
+
+use clap::Parser;
+use log::{debug, info};
+use tokio::task;
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::error::SendError;
+use tokio::net::UdpSocket;
+use tokio::time::Duration;
+
+use media::Packet;
+use media::{PAYLOAD_SIZE, NACK_PAYLOAD_SIZE};
+
+
+const MPSC_CHANNEL_SIZE: usize = 100;
+
+#[derive(Debug, Parser)]
+struct Cli {
+    /// Port to receive packets on.
+    #[arg(long, default_value_t = 5201)]
+    port: u16,
+    /// Frequency at which to send data packets, in ms.
+    #[arg(long, default_value_t = 20)]
+    frequency: u64,
+}
+
+/// Listen for incoming packets on the UDP socket and handle.
+async fn listen_incoming(
+    tx: mpsc::Sender<(Packet, SocketAddr)>, sock: Arc<UdpSocket>,
+    frequency: Duration,
+) -> io::Result<()> {
+    let mut buf = [0u8; PAYLOAD_SIZE];
+    let mut connections = HashSet::new();
+    loop {
+        // Parse the incoming packet.
+        let (len, addr) = sock.recv_from(&mut buf).await?;
+        let data = Packet::from_payload(&buf);
+        assert!(len == NACK_PAYLOAD_SIZE);
+        assert!(!data.is_init_ack);
+        assert!(data.is_init || data.is_nack);
+
+        // Handle non-data packets.
+        if data.is_init {
+            info!("Send init ACK {:?}", addr);
+            tx.send((Packet::new_init_ack(), addr)).await.unwrap();
+            if !connections.contains(&addr) {
+                connections.insert(addr);
+                let tx = tx.clone();
+                task::spawn(async move {
+                    gen_data_packets(tx, frequency, addr).await.unwrap();
+                });
+            }
+            continue;
+        }
+
+        // Retransmit data if it's a NACK.
+        if data.is_nack {
+            debug!("retransmit data {}", data.seqno);
+            let retx = Packet::new_data(data.seqno);
+            tx.send((retx, addr.clone())).await.unwrap();
+            continue;
+        }
+    }
+}
+
+/// Send outgoing packets on the UDP socket based on the mpsc channel.
+async fn send_outgoing(
+    mut rx: mpsc::Receiver<(Packet, SocketAddr)>, sock: Arc<UdpSocket>,
+    bound: bool,
+) -> io::Result<()> {
+    let mut payload = [0xFF; PAYLOAD_SIZE];
+    while let Some((packet, to)) = rx.recv().await {
+        let len = packet.fill_payload(&mut payload);
+        if bound {
+            sock.send(&payload[..len]).await.unwrap();
+        } else {
+            sock.send_to(&payload[..len], to).await.unwrap();
+        }
+    }
+    Ok(())
+}
+
+/// Generate a stream of media packets at the specified frequency. When the
+/// timeout is reached, send several timeout packets and return.
+async fn gen_data_packets(
+    tx: mpsc::Sender<(Packet, SocketAddr)>,
+    frequency: Duration, to: SocketAddr,
+) -> Result<(), SendError<(Packet, SocketAddr)>> {
+    let mut interval = tokio::time::interval(frequency);
+    for seqno in 1..u32::MAX {
+        interval.tick().await;
+        debug!("send data {}", seqno);
+        let data = Packet::new_data(seqno);
+        tx.send((data, to.clone())).await?;
+    }
+
+    Ok(())
+}
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> io::Result<()> {
+    env_logger::init();
+    let args = Cli::parse();
+    let frequency = Duration::from_millis(args.frequency);
+
+    // Bind to the local socket to listen to and send packets from.
+    let sock = Arc::new(UdpSocket::bind(format!("0.0.0.0:{}", args.port)).await?);
+    info!("Ready to accept incoming packets {:?}", sock.local_addr());
+
+    // Channel for sending data on the UDP socket from one thread.
+    let (tx, rx) = mpsc::channel(MPSC_CHANNEL_SIZE);
+    {
+        let sock = sock.clone();
+        task::spawn(async move { send_outgoing(rx, sock, false).await.unwrap() });
+    }
+
+    // // Send data packets to the multicast proxy.
+    // {
+    //     let tx = tx.clone();
+    //     task::spawn(async move {
+    //         gen_data_packets(tx, frequency, args.addr).await.unwrap();
+    //     });
+    // }
+
+    // Start the server.
+    listen_incoming(tx, sock, frequency).await.unwrap();
+    Ok(())
+}
