abfcbf74fe08c072fc8f75e5eeab2d004d77ffa1
Gina Yuan <gina.yuan@gmail.com>
2025-03-13 15:16:31 -0400

    Selectively print categories for cycles counted

--- proxy/src/cycles.rs
+++ proxy/src/cycles.rs
@@ -1,9 +1,12 @@
-const NUM_MEASUREMENTS: usize = 11;
+const NUM_MEASUREMENTS: usize = 14;
 const PRINT_NUM_PACKETS: u64 = 1000;
 const HEADERS: [&str; NUM_MEASUREMENTS] = [
     "handle_packet", "basectos", "basestoc", "sidekick", "none", "encode",
     "deserialize", "decode", "cache_add", "retransmit", "cache_evict",
+    "evict_missing", "evict_received", "drain",
+
 ];
+const PRINT_INDEXES: [usize; 9] = [3, 6, 7, 8, 9, 10, 11, 12, 13];
 
 static mut START: [u64; NUM_MEASUREMENTS] = [0; NUM_MEASUREMENTS];
 static mut CYCLES: [u64; NUM_MEASUREMENTS] = [0; NUM_MEASUREMENTS];
@@ -11,8 +14,9 @@ static mut NUM_PACKETS: [u64; NUM_MEASUREMENTS] = [0; NUM_MEASUREMENTS];
 
 unsafe fn print_cycles_summary() {
     let total = NUM_PACKETS[0];
-    let cycles_norm = (0..NUM_MEASUREMENTS)
-        .map(|i| if NUM_PACKETS[i] == 0 {
+    let cycles_norm = PRINT_INDEXES
+        .iter()
+        .map(|&i| if NUM_PACKETS[i] == 0 {
             0
         } else {
             CYCLES[i] / NUM_PACKETS[i]
@@ -20,15 +24,20 @@ unsafe fn print_cycles_summary() {
         .map(|cycles| cycles.to_string())
         .collect::<Vec<_>>()
         .join(",");
-    let count_prop = NUM_PACKETS
+    let count_prop = PRINT_INDEXES
         .iter()
-        .skip(1)
-        .map(|&count| (count as f64) / (total as f64))
+        .map(|&i| NUM_PACKETS[i])
+        .map(|count| (count as f64) / (total as f64))
         .map(|count| format!("{:.2}", count))
         .collect::<Vec<_>>()
         .join(",");
     if total == PRINT_NUM_PACKETS {
-        println!("{}", HEADERS.join(","));
+        let headers = PRINT_INDEXES
+            .iter()
+            .map(|&i| HEADERS[i])
+            .collect::<Vec<_>>()
+            .join(",");
+        println!("{}", headers);
     }
     println!("{} (total={},count={})", cycles_norm, total, count_prop);
 }
