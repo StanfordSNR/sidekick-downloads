7d0e2b2a18682eb7d6a2c4dc5d41993b9c2d7af8
Gina Yuan <gina.yuan@gmail.com>
2025-04-08 23:58:39 +0000

    Reduce the default reset freq so it's closer to the sidekick RTT 2ms

--- sidekick_utils/src/packet/reset.rs
+++ sidekick_utils/src/packet/reset.rs
@@ -24,8 +24,8 @@ use serde::{Deserialize, Serialize};
 const MAGIC: [u8; 6] = *b"SRESET";
 
 /// Suggested number of ms to wait for between sending resets if the state is
-/// still not consistent
-pub const RESET_FREQ_MS: u64 = 50;
+/// still not consistent. Must be at least the RTT.
+pub const RESET_FREQ_MS: u64 = 5;
 
 /// The UDP payload of a Reset packet from the quACK receiver.
 #[derive(Debug, Serialize, Deserialize)]
