ffdc1178a75cc0f6317e4b77ed023208bc551a49
Gina Yuan <gina.yuan@gmail.com>
2025-04-14 16:22:14 +0000

    Bug in setting frequency in picoquic treatments :( but looks ok now

--- notebook/treatments/picoquic.py
+++ notebook/treatments/picoquic.py
@@ -1,8 +1,8 @@
 from typing import Optional
 from experiment import Treatment
 
-DEFAULT_FREQ_MS = 25
-DEFAULT_FREQ_PKTS = 8
+DEFAULT_FREQ_MS = 10
+DEFAULT_FREQ_PKTS = 16
 DEFAULT_THRESHOLD = lambda freq_pkts: freq_pkts * 5 // 2
 IBLT_MULTIPLIER = 4
 PROTOCOL = 'picoquic'
@@ -12,7 +12,7 @@ def nos(iblt: bool=False, hint: bool=False, cache_capacity: Optional[int]=None, 
     if not freq_pkts:
         freq_pkts = DEFAULT_FREQ_PKTS
     threshold = DEFAULT_THRESHOLD(freq_pkts)
-    options += ['--freq-ms', str(freq_pkts), '--freq-pkts', str(DEFAULT_FREQ_MS)]
+    options += ['--freq-pkts', str(freq_pkts), '--freq-ms', str(DEFAULT_FREQ_MS)]
     if iblt:
         options += ['--threshold', str(threshold * IBLT_MULTIPLIER), '--riblt']
     else:
@@ -52,15 +52,15 @@ def generate_treatments():
         Treatment(PROTOCOL, label=f'picoquic', network_options=[], protocol_options=[]),
         Treatment(PROTOCOL, label=f'picoquic_30ms', network_options=[], protocol_options=pos(30)),
         Treatment(PROTOCOL, label=f'picoquic_split', network_options=['--proxy', 'picoquic'], protocol_options=[]),
-        generate_treatment('iblt', 30, True, freq_pkts=10, reset=True),
-        generate_treatment('iblt', 30, True, freq_pkts=10, cache_capacity=48000, reset=True),
-        generate_treatment('iblt', 30, True, freq_pkts=10, cache_capacity=16000, reset=True),
-        generate_treatment('iblt', 30, True, freq_pkts=10, cache_capacity=48000),
-        generate_treatment('iblt', 30, True, freq_pkts=10, cache_capacity=16000),
+        generate_treatment('iblt', 30, True, reset=True),
+        generate_treatment('iblt', 30, True, cache_capacity=48000, reset=True),
+        generate_treatment('iblt', 30, True, cache_capacity=16000, reset=True),
+        generate_treatment('iblt', 30, True, cache_capacity=48000),
+        generate_treatment('iblt', 30, True, cache_capacity=16000),
     ]
     for ty in ['sidekick', 'iblt']:
         for delay in [0, 5, 10, 20, 30, 60]:
-            for freq in [None, 8, 10, 16]:
+            for freq in [None, 8, 10, 16, 32]:
                 treatments.append(generate_treatment(ty, delay, False, freq_pkts=freq))
                 treatments.append(generate_treatment(ty, delay, True, freq_pkts=freq))
     labels = [treatment.label() for treatment in treatments]
