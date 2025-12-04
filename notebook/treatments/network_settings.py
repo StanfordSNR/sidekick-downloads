4fb9cac2802526cec4e5a43836e238e0563df44b
Thea Rossman <thearossman@gmail.com>
2025-12-03 18:27:19 -0700

    add logic for fixing GE loss

--- notebook/treatments/network_settings.py
+++ notebook/treatments/network_settings.py
@@ -25,3 +25,57 @@ def network_settings(loss_list):
                     delay2=ns.get('delay2'), loss1=loss, loss2=0))
     return settings
 
+# Gilbert-Elliott Setup
+
+import numpy as np
+# overall_loss_pct will fix stationary/long-running
+# probabilities for `good` and `bad` states
+# define `p` and `r` as (scaling_factor * desired_stationary_probability)
+# larger scaling factor = shorter bursts; smaller scaling factor = longer bursts
+scale_by = np.linspace(0.95, 0.01, 10)
+# Fix good and bad state quality
+loss_good_pct = 2
+loss_bad_pct = 80
+loss_good = loss_good_pct / 100.0
+loss_bad = loss_bad_pct / 100.0
+
+# ge=p,r,loss_bad,loss_good
+# overall loss should be passed in as a %
+def ge_configs(overall_loss_pct):
+    overall_loss = overall_loss_pct / 100.0
+    settings = []
+
+    # stationary probabilities per state
+    # overall_loss = p_good * loss_good + p_bad * loss_bad
+    p_bad = (overall_loss - loss_good) / (loss_bad - loss_good)
+    p_good = 1 - p_bad
+
+    # IID loss
+    settings.append(f"1,0,{overall_loss_pct},{overall_loss_pct}")
+    for scale in scale_by:
+        # transition probabilities
+        p_pct = round(p_bad * scale * 100, 2)
+        r_pct = round(p_good * scale * 100, 2)
+        settings.append(f"{p_pct},{r_pct},{loss_bad},{loss_good}")
+
+    return settings
+
+def network_settings_ge(loss):
+    settings = {}
+    for (label, ns) in NS_LABELS.items():
+        settings[label] = []
+        ge = ge_configs(loss)
+        for ge_ in ge:
+            settings[label].append(
+                NetworkSetting(bw1=ns.get('bw1'), bw2=ns.get('bw2'), delay1=ns.get('delay1'),
+                    delay2=ns.get('delay2'), ge=ge_))
+    return settings
+
+def ge_to_burst_size(ge_str):
+    parts = ge_str.split(',')
+    p = float(parts[0]) / 100.0
+    r = float(parts[1]) / 100.0
+    if p == 1.0:
+        return 1.0  # IID loss
+    avg_bad_length = 1.0 / r
+    return (1.0 / r) * 100
